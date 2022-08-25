5dedad1ef394b9a2a64bb30f72cc2bdcd348c659
ngovantrinh <trinhtamtao@gmail.com>
2022-08-25 09:32:55 +0700

    create prj

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- <input type="file" name="" id="" @change="onChangeFile" /> -->
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup() {
+    function onChangeFile(e) {
+      console.log('HelloWorld', e);
+    }
+    return {
+      onChangeFile,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
